c97804617190fba3aac5e47a54f5e98043b900a1
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-05-21 17:01:31 -0300

    feat: add Subscribe model

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from .base import Base
+from .newsletter import Subscriber
+
+
+__all__ = [
+    "Base",
+    "Subscriber",
+]
---
+++ app/models/newsletter.py
@@ -0,0 +1,32 @@
+import uuid
+from datetime import datetime
+
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models.base import Base
+
+
+class Subscriber(Base):
+    __tablename__ = "subscribers"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        sa.UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    email: Mapped[str] = mapped_column(
+        sa.String(255), unique=True, nullable=False, index=True
+    )
+    subscribed: Mapped[str] = mapped_column(
+        sa.Integer, nullable=True, default=1
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        sa.DateTime(timezone=True),
+        server_default=sa.func.now(),
+        nullable=False,
+    )
+    unsubscribed_at: Mapped[datetime] = mapped_column(
+        sa.DateTime(timezone=True),
+        nullable=True,
+    )
