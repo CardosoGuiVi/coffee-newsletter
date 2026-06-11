4589055cddb1c0fb9ad491d6de6683377f3e05ae
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-06-10 21:03:19 -0300

    refactor: define default values and improve `URI` property

--- packages/database/schemas.py
+++ packages/database/schemas.py
@@ -4,14 +4,15 @@ from pydantic import BaseModel
 class DatabaseSettings(BaseModel):
     HOST: str = "localhost"
     PORT: int = 5432
-    USER: str
-    DB: str
-    PASSWORD: str
+    USER: str = "local_user"
+    DB: str = "local_db"
+    PASSWORD: str = "local_password"
 
     @property
     def URI(self) -> str:
         return (
-            f"postgresql+asyncpg://{self.USER}:"
-            f"{self.PASSWORD}@{self.HOST}:"
-            f"{self.PORT}/{self.DB}"
+            f"postgresql+asyncpg://"
+            f"{self.USER}:{self.PASSWORD}"
+            f"@{self.HOST}:{self.PORT}"
+            f"/{self.DB}"
         )
